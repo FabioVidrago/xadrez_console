f35309f70433a72c5947f74ea51c742307d34b92
FabioVidrago <vidragosvsky@hotmail.com>
2023-01-12 22:37:53 +0000

    criada a posicao do tabuleiro de xadrez

--- Program.cs
+++ Program.cs
@@ -10,13 +10,18 @@ namespace xadrez_console
         {
             try
             {
+                /*
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
                 tab.colocarPeca(new Torre(tab, Cor.Vermelha), new Posicao(0, 0));
                 tab.colocarPeca(new Torre(tab, Cor.Vermelha), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Amarelo), new Posicao(0, 9));
+                tab.colocarPeca(new Rei(tab, Cor.Amarelo), new Posicao(0, 2));
 
-                Tela.imprimirTabuleiro(tab);
+                Tela.imprimirTabuleiro(tab);*/
+
+                PosicaoXadrez pos = new PosicaoXadrez('c',7);
+                Console.WriteLine(pos);
+                Console.WriteLine(pos.toPosicao());
 
                 Console.ReadLine();
             }
