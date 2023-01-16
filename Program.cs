80b16cd20528345a159607fa73dfd6aec34b62e1
FabioVidrago <vidragosvsky@hotmail.com>
2023-01-16 21:39:32 +0000

    testes de movimentação

--- Program.cs
+++ Program.cs
@@ -13,10 +13,21 @@ namespace xadrez_console
 
                 PartidaDeXadrez partida = new PartidaDeXadrez();
 
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
+
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.ExecutaMovimento(origem, destino);
+                }
+
                 
-
-
-                Tela.imprimirTabuleiro(partida.tab);
 
                 /*PosicaoXadrez pos = new PosicaoXadrez('c',7);
                 Console.WriteLine(pos);
